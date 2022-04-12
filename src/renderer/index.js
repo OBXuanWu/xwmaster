700bfac68a79b678bc33a7227cfc4e206d658eca
linjingxiong <linjingxiong@orbbec.com>
2022-04-12 17:53:56 +0800

    初始化 Vue + Electron 工程

---
+++ src/renderer/index.js
@@ -0,0 +1,59 @@
+// Vue entry file / vue入口文件
+
+// Depends ------
+import Vue from 'vue'
+import App from 'renderer/App.vue'
+import router from 'renderer/router'
+import store from 'renderer/store'
+
+// Components ------
+import SvgIcon from 'components/svgIcon/index.vue'
+import SvgIconList from 'components/svgIcon'
+
+// Plugins ------
+import i18n from 'plugins/i18n'
+import Bus from 'plugins/bus'
+
+// Backend ------
+import $backend from 'backend'
+
+// Less or Css ------
+/* Nothing now */
+
+import components from 'ob-common-components'
+import 'ob-common-components/lib/ob-common-components.css'
+// import { PointCloudView, DepthView } from 'ob-common-components'
+
+const PointCloudView = components[0]
+const DepthView = components[1]
+const ColorView = components[2]
+const LogView = components[3]
+
+Vue.use(PointCloudView)
+Vue.use(DepthView)
+Vue.use(ColorView)
+Vue.use(LogView)
+
+// Add Prototype ------
+Vue.prototype.$backend = $backend
+Vue.prototype.SvgIconList = SvgIconList
+Vue.prototype.$electron = require('electron') // You can access electron object with `this.$electron` in renderer process
+
+Vue.config.productionTip = false
+
+// Use plugins
+Vue.use(Bus, { router })
+
+// Use components ------
+Vue.component('SvgIcon', SvgIcon)
+
+// Vue options
+const options = {
+  router,
+  store,
+  i18n,
+  render: h => h(App)
+}
+
+// Finally, returns the new vue instance
+new Vue(options).$mount('#app')
